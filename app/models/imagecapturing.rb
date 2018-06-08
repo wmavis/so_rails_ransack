7ad1a20eccede13b5426ae5aa532eac63a8d89d5
Will Mavis <willard.mavis@gmail.com>
2018-06-08 09:59:52 -0500

    Use scopes to filter by year

--- app/models/imagecapturing.rb
+++ app/models/imagecapturing.rb
@@ -1,2 +1,10 @@
 class Imagecapturing < ApplicationRecord
+  # https://github.com/activerecord-hackery/ransack/#using-scopesclass-methods
+  def self.finish_year(year)
+    where("strftime('%Y', finish_date) = ?", year)
+  end
+
+  def self.ransackable_scopes(auth_object = nil)
+    %i(finish_year)
+  end
 end
